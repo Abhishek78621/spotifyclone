9505efe1e3b8b04232c172411f122ce0dd0c7997
Abhishek <abhishek421486@gmail.com>
2024-06-21 16:55:00 +0530

    updated

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.js'
+import './index.css'
+import {HashRouter} from 'react-router-dom'
+import PlayerContextProvider from './Contexts/PlayerContext.js'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+    <PlayerContextProvider>
+    <App />
+    </PlayerContextProvider>
+   
+    </HashRouter>
+  </React.StrictMode>,
+)
